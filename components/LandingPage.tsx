d7dccedb9d9a3ea10121751137395aeaaedca9bb
Mo Azza <ssbsdmg@gmail.com>
2025-01-27 16:33:34 +0200

    Update LandingPage.tsx

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -172,7 +172,7 @@ export default function LandingPage() {
                 FROM CAMPFIRE TO COMMUNITY
               </h1>
               <h3 className="text-xl md:text-2xl font-medium text-white/90 mb-8 tracking-wide text-center px-4 [text-shadow:_0_2px_2px_rgb(0_0_0_/_30%)]">
-                BUILD A THRIVING TOWN ON THE BLOCKCHAIN
+                BUILD A THRIVING AI TOWN ON THE BLOCKCHAIN
               </h3>
             </div>
           </main>
